8c23138c4cb0aa821cef720d07f519c98971a990
delphie23 <arielgrunfeld@gmail.com>
2014-07-17 01:12:06 +0300

    Mouse movement listener.

--- LogisticModel/src/GUI.java
+++ LogisticModel/src/GUI.java
@@ -7,29 +7,30 @@ import javax.swing.event.*;
 public class GUI {
 	static final String FRAME_LABEL = "Logistic Model";
 	static final String INIT_LABEL = "Initial iterations:";
-	static final String X_RANGE_LABEL = "X resolution:";
 	static final String R_RANGE_LABEL = "R resolution:";
+	static final String X_RANGE_LABEL = "X resolution:";
 	static final String RESET_LABEL = "Reset";
 	static final int INIT_MIN = 0;
 	static final int INIT_MAX = 10000;
 	static final int INIT_INIT = 1000;
-	static final int X_RES_MIN = 1;
-	static final int X_RES_MAX = 10000;
-	static final int X_RES_INIT = 1000;
 	static final int R_RES_MIN = 1000;
 	static final int R_RES_MAX = 10000;
 	static final int R_RES_INIT = 3000;
+	static final int X_RES_MIN = 1;
+	static final int X_RES_MAX = 10000;
+	static final int X_RES_INIT = 1000;
+	
 	
 	/*TODO: Remake this so the user will control those values.*/
 	private double x0 = 0.001;
-	private double minX = 0.0;	// At least 0.0
-	private double maxX = 1.0;	// At most 1.0
 	private double minR = 3.55;	// At least 0.0
 	private double maxR = 4.0;	// At most 4.0
+	private double minX = 0.0;	// At least 0.0
+	private double maxX = 1.0;	// At most 1.0
 	
 	private int initValue = INIT_INIT;
-	private int xResValue = X_RES_INIT;
 	private int rResValue = R_RES_INIT;
+	private int xResValue = X_RES_INIT;
 	
 	private JFrame mainFrame; // The main panel.
 	private JPanel statusBar;
@@ -37,12 +38,13 @@ public class GUI {
 	private Diagram diagram; // Bifurcation Diagram. REPLACE
 	private JLabel statusLabel; // Should read the coordinates of the mouse.
 	private JLabel initLabel;
-	private JLabel xRangeLabel;
 	private JLabel rRangeLabel;
+	private JLabel xRangeLabel;
 	private JButton resetButton;
 	private JSlider initSlider;
+	private JSlider rResSlider;
 	private JSlider xResSlider;
-	private JSlider rResSlider;  
+	  
 	
 	public static void main(String[] args){
 		new GUI();
@@ -59,8 +61,8 @@ public class GUI {
 		
 		statusLabel = new JLabel("",JLabel.CENTER);
 		initLabel = new JLabel(INIT_LABEL,JLabel.RIGHT);
-		xRangeLabel = new JLabel(X_RANGE_LABEL, JLabel.RIGHT);
 		rRangeLabel = new JLabel(R_RANGE_LABEL, JLabel.RIGHT);
+		xRangeLabel = new JLabel(X_RANGE_LABEL, JLabel.RIGHT);
 		
 		resetButton = new JButton(RESET_LABEL);
 		
@@ -69,16 +71,16 @@ public class GUI {
 		initSlider.setMinorTickSpacing(1000);
 		initSlider.setPaintTicks(true);
 		initSlider.setPaintLabels(true);
-		xResSlider = new JSlider(JSlider.HORIZONTAL, X_RES_MIN, X_RES_MAX, X_RES_INIT);
-		xResSlider.setMajorTickSpacing(5000);
-		xResSlider.setMinorTickSpacing(1000);
-		xResSlider.setPaintTicks(true);
-		xResSlider.setPaintLabels(true);
 		rResSlider = new JSlider(JSlider.HORIZONTAL, R_RES_MIN, R_RES_MAX, R_RES_INIT);
 		rResSlider.setMajorTickSpacing(5000);
 		rResSlider.setMinorTickSpacing(1000);
 		rResSlider.setPaintTicks(true);
 		rResSlider.setPaintLabels(true);
+		xResSlider = new JSlider(JSlider.HORIZONTAL, X_RES_MIN, X_RES_MAX, X_RES_INIT);
+		xResSlider.setMajorTickSpacing(5000);
+		xResSlider.setMinorTickSpacing(1000);
+		xResSlider.setPaintTicks(true);
+		xResSlider.setPaintLabels(true);
 		
 		mainFrame.add(statusBar,BorderLayout.SOUTH);
 		mainFrame.add(controlBar, BorderLayout.NORTH);
@@ -89,18 +91,18 @@ public class GUI {
 		controlBar.setLayout(new GridLayout(1,7));
 		controlBar.add(initLabel);
 		controlBar.add(initSlider);
-		controlBar.add(xRangeLabel);
-		controlBar.add(xResSlider);
 		controlBar.add(rRangeLabel);
 		controlBar.add(rResSlider);
+		controlBar.add(xRangeLabel);
+		controlBar.add(xResSlider);
 		controlBar.add(resetButton);
 		
 		initSlider.addChangeListener(new SliderListener());
 		initSlider.setName(INIT_LABEL);
-		xResSlider.addChangeListener(new SliderListener());
-		xResSlider.setName(X_RANGE_LABEL);
 		rResSlider.addChangeListener(new SliderListener());
 		rResSlider.setName(R_RANGE_LABEL);
+		xResSlider.addChangeListener(new SliderListener());
+		xResSlider.setName(X_RANGE_LABEL);
 		resetButton.addActionListener(new ButtonListener());
 		resetButton.setActionCommand("reset");
 		diagram.addMouseMotionListener(new MotionListener());
@@ -138,7 +140,21 @@ public class GUI {
 	private class MotionListener implements MouseMotionListener {
 		@Override
 		public void mouseDragged(MouseEvent e) {
-			statusLabel.setText(""+e.getX());
+			Object source = e.getSource();
+			if (source == diagram) {
+				Rectangle b = diagram.getBounds();
+				int maxCanvasX = (int)b.getMaxX();
+				int maxCanvasY = (int)b.getMaxY();
+				//double r = minR + e.getX()*maxR/maxCanvasX;
+				//double r = e.getX()/(double)maxCanvasX;
+				double r = minR + (maxR-minR)*e.getX()/maxCanvasX;
+				if (r<minR) r = minR;
+				if (r>maxR) r  = maxR;
+				double x = maxX + (minX-maxX)*e.getY()/maxCanvasY;
+				if (x<minX) x = minX;
+				if (x>maxX) x = maxX;
+				statusLabel.setText("r="+r+", x="+x);
+			}
 		}
 
 		@Override
